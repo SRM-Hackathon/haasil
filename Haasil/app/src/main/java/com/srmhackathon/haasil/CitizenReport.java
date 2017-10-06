17e3bbb85b5bfbf1fa9de2b38f973c77f136e287
Ravi Tripathi <ravitripathi1996@gmail.com>
2017-10-06 22:52:01 +0530

    Added Both the codes up

--- Haasil/app/src/main/java/com/srmhackathon/haasil/CitizenReport.java
+++ Haasil/app/src/main/java/com/srmhackathon/haasil/CitizenReport.java
@@ -52,3 +52,4 @@ public class CitizenReport extends AppCompatActivity {
         });
 
     }
+}
--- Haasil/app/src/main/java/com/srmhackathon/haasil/IntroActivity.java
+++ Haasil/app/src/main/java/com/srmhackathon/haasil/IntroActivity.java
@@ -1,5 +1,6 @@
 package com.srmhackathon.haasil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,10 +13,11 @@ import java.util.List;
 public class IntroActivity extends OnboarderActivity {
 
     List<OnboarderPage> onboarderPages;
-
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        i = new Intent(this,Login.class);
         OnboarderPage onboarderPage1 = new OnboarderPage("EFFORTLESSLY", "Our lovely pale blu", R.drawable.slide1);
         OnboarderPage onboarderPage2 = new OnboarderPage("Venus", "The love goddess", R.drawable.slide2);
         OnboarderPage onboarderPage3 = new OnboarderPage("Mars", "Say hi to Curiosity!", R.drawable.slide3);
@@ -46,11 +48,15 @@ public class IntroActivity extends OnboarderActivity {
     public void onSkipButtonPressed() {
         // Optional: by default it skips onboarder to the end
         super.onSkipButtonPressed();
+
+        startActivity(i);
         // Define your actions when the user press 'Skip' button
     }
 
     @Override
     public void onFinishButtonPressed() {
+
+        startActivity(i);
         // Define your actions when the user press 'Finish' button
     }
 
